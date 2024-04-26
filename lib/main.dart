29acd73bd81cfa415663d39c34c26817e066fa94
Jonathan Ixcayau <jonynija@gmail.com>
2024-04-26 11:19:58 -0600

    Add sentry and error handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,34 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'package:alertify/app.dart';
 import 'package:alertify/firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  runZonedGuarded(
+    () async {
+      await SentryFlutter.init(
+        (options) {
+          options.dsn =
+              'https://45708111855b9caac650f65f8e0c14fb@o4506367206948864.ingest.us.sentry.io/4507153958305792';
+          options.environment = 'dev';
+        },
+      );
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+      WidgetsFlutterBinding.ensureInitialized();
+
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+
+      runApp(const MainApp());
+    },
+    (exception, stackTrace) async {
+      await Sentry.captureException(exception, stackTrace: stackTrace);
+    },
   );
-
-  runApp(const MainApp());
 }
